e80b67cac86adbdfee5aa35ab9e44271a4f02e93
halkemist <swierz.tony@gmail.com>
2025-03-12 17:01:06 +0100

    backend: initialize availability controllers, create Availability request

---
+++ reserve-link/app/Http/Controllers/AvailabilitySlotController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Manage slot booking (guest and auth users).
+ */
+class AvailabilitySlotController extends Controller
+{
+    //
+}
