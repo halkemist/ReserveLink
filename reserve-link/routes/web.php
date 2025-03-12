bfcaa9aa76ec23b19e2436ff4087ec8890026bf8
halkemist <swierz.tony@gmail.com>
2025-03-12 16:07:58 +0100

    core: PHPMyAdmin installation

--- reserve-link/routes/web.php
+++ reserve-link/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AvailabilityController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    return 'hello';
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +20,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/availabilities', [AvailabilityController::class, 'index']);
+    Route::post('/availabilities', [AvailabilityController::class, 'store']);
 });
 
 require __DIR__.'/auth.php';
