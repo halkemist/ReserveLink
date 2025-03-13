80d555131e80979c7d9cbdb7226c23d7df3207ab
halkemist <swierz.tony@gmail.com>
2025-03-13 10:32:49 +0100

    routes: refacto; dashboardcontroller: index method to return user availabilities to edit

---
+++ reserve-link/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AvailabilityRequest;
+use App\Models\Availability;
+use App\Models\Booking;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Manage availability slots (authentified user).
+ */
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        
+        // Get incoming meeting
+        $upcomingBookings = Booking::where(function($query) use ($user) {
+                $query->where('owner_id', $user->id)
+                    ->orWhere('user_id', $user->id);
+            })
+            ->where('start_time', '>', now())
+            ->orderBy('start_time', 'asc')
+            ->get();
+        
+        // Get availabilities
+        $availabilities = Availability::where('user_id', $user->id)
+            ->orderBy('day_of_week')
+            ->orderBy('start_time')
+            ->get();
+        
+        // Week days
+        $dayNames = [
+            0 => 'Dimanche',
+            1 => 'Lundi',
+            2 => 'Mardi',
+            3 => 'Mercredi',
+            4 => 'Jeudi',
+            5 => 'Vendredi',
+            6 => 'Samedi',
+        ];
+        
+        return view('dashboard', compact('upcomingBookings', 'availabilities', 'dayNames'));
+    }
+
+    public function store(AvailabilityRequest $request)
+    {
+        $authUser = Auth::user();
+        $authUser->availabilities->create($request->validated());
+    }
+}
--- reserve-link/routes/web.php
+++ reserve-link/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AvailabilityController;
 use App\Http\Controllers\AvailabilitySlotController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,27 +9,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return 'hello';
-});
-
+// Availabilities
 Route::get('/availabilities/{email}', [AvailabilitySlotController::class, 'showUserAvailabilities']);
 
+// Booking
 Route::get('/booking/create', function () {
     return 'reservation done';
 })->name('booking.create');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/availabilities', [AvailabilityController::class, 'index']);
-    Route::post('/availabilities', [AvailabilityController::class, 'store']);
 });
 
 require __DIR__.'/auth.php';
